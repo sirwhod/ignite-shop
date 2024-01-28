545d94c68839acaa82e41daf5c0f20b487573b0c
Rodrigo <rodrigo.brandao98@gmail.com>
2024-01-27 21:21:25 -0300

    class: Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,33 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src="" alt="" />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam
+          consequuntur assumenda facere ab! Unde alias, animi, quaerat sit
+          reiciendis, doloribus exercitationem repellendus dolorum esse maiores
+          provident voluptates a. Vero, minus.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
